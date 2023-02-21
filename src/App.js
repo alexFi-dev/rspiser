d70d4eae5b43f4251a6ce2690f368c3ca4dbc729
alexFi-dev <alexfi.dev@gmail.com>
2023-02-21 23:19:40 +0700

    My sad not ready dapp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+import '@rainbow-me/rainbowkit/styles.css';
+
+import { ConnectButton, RainbowKitProvider, lightTheme } from '@rainbow-me/rainbowkit';
+import { ChakraProvider, Container, Heading, extendTheme, Box, Flex, Button, VisuallyHidden } from '@chakra-ui/react';
+import { useAccount } from 'wagmi';
+
+import PlayGame from './components/PlayGame';
+
+const theme = extendTheme({
+  styles: {
+    global: () => ({
+      body: {
+        bg: '#fdffcd',
+      }
+    })
+  },
+})
+
+function App(props) {
+  const {data: accountdata} = useAccount();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RainbowKitProvider chains={props.chains} theme={lightTheme({
+      accentColor: 'red'
+    })}>
+      <ChakraProvider theme={theme}>
+        <Container maxW='xl' background="#fdffcd">
+
+          <Heading textAlign="center" pt="10">tasty DApp 🤑</Heading>
+          <Box display="flex" alignItems="center" justifyContent="center" p="5">
+            <ConnectButton />
+            
+          </Box>
+          
+
+          <PlayGame />
+        
+          
+        </Container>
+      </ChakraProvider>
+    </RainbowKitProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import { getDefaultWallets } from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { bscTestnet } from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, provider } = configureChains(
+  [bscTestnet],
+  [
+    alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <WagmiConfig client={wagmiClient}>
+      <App chains={chains} provider={provider} />
+    </WagmiConfig>
+  </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
